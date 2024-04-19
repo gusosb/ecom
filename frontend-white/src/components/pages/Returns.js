20fd3e9a1857f658aaff15f4a0257560c8e60f49
Gustav Osbeck <osbeckgustav@gmail.com>
2024-04-19 07:43:05 +0200

    update returns with selectedCurrency

--- frontend-white/src/components/pages/Returns.js
+++ frontend-white/src/components/pages/Returns.js
@@ -1,10 +1,13 @@
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography';
 import { useOutletContext } from "react-router-dom";
+import { useCountryCurrency } from '../../helpers';
 
 const Returns = () => {
 
     const [windowSize] = useOutletContext();
+
+    const { selectedCurrency } = useCountryCurrency();
 
     return (
         <>
@@ -13,7 +16,7 @@ const Returns = () => {
             </Box>
 
             <Box mx={windowSize > 800 ? 20 : 5} mt={4}>
-                <Typography variant="body1" gutterBottom>You are free to return your order within 30 days, providing the garments are unused, contact us by email at <a href='mailto:info@gustaflund.com'>info@gustaflund.se</a> for help with returning your order. When you return an order we charge a shipping fee of 100 SEK / 10 EUR.</Typography>
+                <Typography variant="body1" gutterBottom>You are free to return your order within 30 days, providing the garments are unused, contact us by email at <a href='mailto:info@gustaflund.com'>info@gustaflund.se</a> for help with returning your order. When you return an order we charge a shipping fee of {selectedCurrency === 'SEK' ? 100 : 10} {selectedCurrency}.</Typography>
             </Box>
         </>
     );
