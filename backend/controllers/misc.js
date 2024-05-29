748e15b17aa03af96c5a6b443c0d59b30923799e
gusosb <gustav.osbeck@sourceability.com>
2024-05-29 10:25:13 +0200

    everything.

--- backend/controllers/misc.js
+++ backend/controllers/misc.js
@@ -1,16 +1,35 @@
-const miscRouter = require('express').Router();
+const nodemailer = require("nodemailer");
+const miscRouter = require("express").Router();
 
+miscRouter.post("/email", async (request, response) => {
+  const { email } = request.body;
 
+  const transporter = nodemailer.createTransport({
+    host: "smtp.strato.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "admin@evocorp.se",
+      pass: "dfx*PAF*dqy9mkw-dca",
+    },
+  });
 
-miscRouter.post('/email', async (request, response) => {
+  const mailOptions = {
+    from: "admin@evocorp.se",
+    to: "gustav@evocorp.se",
+    subject: "New evocorp.se contact request",
+    text: `email provided: ${email}`,
+  };
 
-    const { email } = request.body;
-
-
-
-    response.status(201).json();
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      response.status(500).json();
+    } else {
+      console.log("Email sent: " + info.response);
+      response.status(201).json();
+    }
+  });
 });
-
-
 
 module.exports = miscRouter;
